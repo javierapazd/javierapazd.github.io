2001cb57a45464e01da461018404f545812f4ef3
aassriv <aassriv@hotmail.com>
2022-07-19 16:09:51 -0400

    arreglo modal index

--- script.js
+++ script.js
@@ -89,3 +89,33 @@ var linkB2 = '${boton2.linkB2.getData()}';
 
         });
 	});
+
+  jQuery(document).ready(function($) {
+    setTimeout(function() {
+      $(".bts-popup").addClass("is-visible");
+    }, 2000);
+  
+    //open popup
+    $(".bts-popup-trigger").on("click", function(event) {
+      event.preventDefault();
+      $(".bts-popup").addClass("is-visible");
+    });
+  
+    //close popup
+    $(".bts-popup").on("click", function(event) {
+      if (
+        $(event.target).is(".bts-popup-close") ||
+        $(event.target).is(".bts-popup")
+      ) {
+        event.preventDefault();
+        $(this).removeClass("is-visible");
+      }
+    });
+    //close popup when clicking the esc keyboard button
+    $(document).keyup(function(event) {
+      if (event.which == "27") {
+        $(".bts-popup").removeClass("is-visible");
+      }
+    });
+  });
+  
